b033905980cb447b2f153d24492720004cfaff94
ayushipandeybharatgroup <112567689+ayushipandeybharatgroup@users.noreply.github.com>
2025-09-10 19:57:47 +0530

    Update variable.tf

--- terraform/vpc_peering/variable.tf
+++ terraform/vpc_peering/variable.tf
@@ -1,14 +1,16 @@
-variable "peer_vpc_id" {
-  description = "The ID of the peer VPC"
-  type        = string
-}
-
 variable "region_name" {
-  description = "The region of the peer VPC"
-  type        = string
+  default = "ap-south-1"
 }
 
-variable "peer_cidr" {
-  description = "The CIDR block of the peer VPC"
-  type        = string
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "default_vpc_cidr" {
+  type = string
+  default = "172.31.0.0/16"
 }
