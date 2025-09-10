5593f31eb091a2a884f4146b5e5c0002236afc17
ayushipandeybharatgroup <112567689+ayushipandeybharatgroup@users.noreply.github.com>
2025-09-10 19:55:52 +0530

    Update main.tf

--- terraform/vpc_peering/main.tf
+++ terraform/vpc_peering/main.tf
@@ -2,49 +2,40 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
-# Create VPC peering connection
-resource "aws_vpc_peering_connection" "main" {
-  vpc_id      = data.aws_vpc.default_vpc.id   # local VPC
-  peer_vpc_id = var.peer_vpc_id               # target VPC (from variable)
-  peer_region = var.region_name
 
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id        = data.aws_vpc.default_vpc.id              
+  peer_vpc_id   = var.vpc_id        
+  peer_region   = var.region_name         
   tags = {
-    Name = "main-vpc-peering"
+    Name = "peering connection"
   }
 }
 
-# Accept the peering connection
 resource "aws_vpc_peering_connection_accepter" "accepter" {
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
   auto_accept               = true
 }
 
-# Get the default route table of the default VPC
 data "aws_route_table" "default_RT" {
   vpc_id = data.aws_vpc.default_vpc.id
   filter {
-    name   = "association.main"
-    values = ["true"]
+    name = "association.main"
+    values = [ "true" ]
+
   }
 }
 
-# Create a route through the peering connection
 resource "aws_route" "default_rt" {
-  route_table_id            = data.aws_route_table.default_RT.id
-  destination_cidr_block    = var.peer_cidr   # pass as variable
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [destination_cidr_block]
-  }
+route_table_id = data.aws_route_table.default_RT.id
+destination_cidr_block = var.vpc_cidr
+vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
 
-# Fetch default security group (if needed)
 data "aws_security_group" "default_sg" {
   vpc_id = data.aws_vpc.default_vpc.id
   filter {
-    name   = "group-name"
-    values = ["default"]
+    name = "group-name"
+    values = [ "default" ]
   }
 }
